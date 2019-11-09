1ce11e89885d1031bde2cbce7323defd19da7083
rajthedev <51006236+rajthedev@users.noreply.github.com>
2019-11-10 10:48:37 +1100

    Add project files.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,68 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        //create webdriver
+        IWebDriver driver = new ChromeDriver();
+        static void Main(string[] args)
+        {
+
+           /* //create webdriver
+            IWebDriver driver = new ChromeDriver();
+
+            //navigate to google page
+
+            driver.Navigate().GoToUrl("http://google.com");
+
+            IWebElement element = driver.FindElement(By.Name("q"));
+
+            element.SendKeys("Execute Automation");
+
+            driver.Close();*/
+        }
+
+        [SetUp]
+        public void Initialise()
+        {
+            //navigate to google page
+
+            driver.Navigate().GoToUrl("http://www.executeautomation.com/demosite/index.html");
+            Console.WriteLine("URL Opened");
+
+        }
+
+        [Test]
+        public void ExecuteTest()
+        {
+            SeleniumSetMethods.DropDown(driver, "TitleId", "Mr.", "Id");
+
+            //Initial
+
+            SeleniumSetMethods.Entertext(driver, "Initial", "Execute Automation", "Name");
+
+            //click
+
+            SeleniumSetMethods.Click(driver, "Save", "Name");
+            Console.WriteLine("test Executed");
+           
+
+        }
+          
+        [TearDown]
+        public void CleanUp()
+        {
+            driver.Close();
+            Console.WriteLine("browser closed");
+
+        }
+    }
+}
---
+++ ConsoleApp2/SeleniumSetMethods.cs
@@ -0,0 +1,42 @@
+﻿using OpenQA.Selenium;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using OpenQA.Selenium.Support.UI;
+
+namespace ConsoleApp2
+{
+    class SeleniumSetMethods
+    {
+        public static void Entertext(IWebDriver driver, string element, string value, string elementtype)
+        {
+            if(elementtype == "Id")
+               driver.FindElement(By.Id(element)).SendKeys(value);
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).SendKeys(value);
+        }
+
+        //click into a button, checkbox, option etc
+        public static void Click(IWebDriver driver, string element, string elementtype)
+        {
+            if (elementtype == "Id")
+                driver.FindElement(By.Id(element)).Click();
+            if (elementtype == "Name")
+                driver.FindElement(By.Name(element)).Click();
+        }
+
+        //selecting a dropdown control
+
+        public static void DropDown(IWebDriver driver, string element, string value, string elementtype)
+        {
+            
+            if (elementtype == "Id")
+                new SelectElement(driver.FindElement(By.Id(element))).SelectByText(value);
+            if (elementtype == "Name")
+                new SelectElement(driver.FindElement(By.Name(element))).SelectByText(value);
+
+        }
+    }
+}
